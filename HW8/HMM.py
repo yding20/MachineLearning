f2bb1423cfb25bf8aa6256e775aecab09a2a81d5
Yanhao Ding <yding20@ur.rochester.edu>
2018-04-19 16:17:22 -0400

    ThirdCommit

--- HW8/HMM.py
+++ HW8/HMM.py
@@ -41,17 +41,24 @@ def Initialize(Xtra, K):
     TransitionA = np.zeros((klen, klen), dtype='float64')+1./klen
     return pi, mu, sigma, TransitionA
  
-def forward_backward(Xtra, pi, mu, sigma, TransitionA):
+def forward_backward(Xtra, Xdev, pi, mu, sigma, TransitionA):
     nlen = len(Xtra[:,0])
+    nlendev = len(Xdev[:,0])
     klen = len(pi)
     alpha = np.zeros((nlen, klen),  dtype='float64')
     scale = np.zeros(nlen,  dtype='float64')
+    alphadev = np.zeros((nlendev, klen),  dtype='float64')
+    scaledev = np.zeros(nlendev,  dtype='float64')
     for k in range (klen):
         gauss = TwoDGaussian(Xtra[0,:], mu[k,:], sigma[k,:,:])
         alpha[0, k] = pi[k]*gauss
-    ##scale[0] = 1
     scale[0] = np.sum(alpha[0, :])
     alpha[0, :] = alpha[0, :] / scale[0]
+
+    for k in range (klen):
+        gauss = TwoDGaussian(Xdev[0,:], mu[k,:], sigma[k,:,:])
+        alphadev[0, k] = pi[k]*gauss
+    scaledev[0] = np.sum(alphadev[0, :])
         
     for n in range (1, nlen):
         for k in range (klen): 
@@ -59,7 +66,12 @@ def forward_backward(Xtra, pi, mu, sigma, TransitionA):
             alpha[n, k] = gauss*sum([alpha[n-1, sumk]*TransitionA[sumk, k] for sumk in range(klen)])
         scale[n] = np.sum(alpha[n,:])/np.sum(alpha[n-1,:])
         alpha[n, :] = alpha[n, :]/ scale[n]
-    
+
+    for n in range (1, nlendev):
+        for k in range (klen): 
+            gauss = TwoDGaussian(Xdev[n,:], mu[k,:], sigma[k,:,:])
+            alphadev[n, k] = gauss*sum([alphadev[n-1, sumk]*TransitionA[sumk, k] for sumk in range(klen)])
+        scaledev[n] = np.sum(alphadev[n,:])/np.sum(alphadev[n-1,:])
     
     beta = np.ones((nlen, klen),  dtype='float64')
     for n in range(nlen-2, -1, -1):
@@ -68,14 +80,15 @@ def forward_backward(Xtra, pi, mu, sigma, TransitionA):
             beta[n, k] = sum([beta[n+1, sumk]*TwoDGaussian(Xtra[n+1,:], mu[sumk,:], sigma[sumk,:,:])*
                                TransitionA[k, sumk] for sumk in range(klen)])
         beta[n, :] =  beta[n, :] / scale[n+1]
-    return alpha, beta, scale
+    return alpha, beta, scale, scaledev
 
-def EM(Xtra, pi, mu, sigma, TransitionA):
+def EM(Xtra, Xdev, pi, mu, sigma, TransitionA):
     nlen = len(Xtra[:,0])
     klen = len(pi)
-    results = []    
-    for interations in range (30):
-        alpha, beta, scale = forward_backward(Xtra, pi, mu, sigma, TransitionA)
+    results = []
+    resultsdev = []    
+    for interations in range (40):
+        alpha, beta, scale, scaledev = forward_backward(Xtra, Xdev, pi, mu, sigma, TransitionA)
         
         ksi = np.zeros((nlen, klen, klen),  dtype='float64')
         gamma = np.ones((nlen, klen),  dtype='float64')
@@ -109,28 +122,37 @@ def EM(Xtra, pi, mu, sigma, TransitionA):
                 tempnominator += gamma[n, k]*np.outer(Xtra[n,:]- mu[k], np.transpose(Xtra[n,:]- mu[k]))
                 tempdenominator += gamma[n,k]
             sigma[k,:,:] = tempnominator/tempdenominator
-        results.append(np.sum(np.log(scale))) 
-    return results[1:]
+        results.append(np.sum(np.log(scale))/9.) 
+        resultsdev.append(np.sum(np.log(scaledev)))
+    return results[1:], resultsdev[1:]
 
 def main():
     Xtra, Xdev = ReadData()
     
     pi, mu, sigma, TransitionA = Initialize(Xtra, 3)
-    Ydata3 = EM(Xtra, pi, mu, sigma, TransitionA)
+    Ydata3, Ydata3dev = EM(Xtra, Xdev, pi, mu, sigma, TransitionA)
 
     pi, mu, sigma, TransitionA = Initialize(Xtra, 4)
-    Ydata4 = EM(Xtra, pi, mu, sigma, TransitionA)
+    Ydata4, Ydata4dev = EM(Xtra, Xdev, pi, mu, sigma, TransitionA)
 
     pi, mu, sigma, TransitionA = Initialize(Xtra, 5)
-    Ydata5 = EM(Xtra, pi, mu, sigma, TransitionA)
+    Ydata5, Ydata5dev = EM(Xtra, Xdev, pi, mu, sigma, TransitionA)
 
     pi, mu, sigma, TransitionA = Initialize(Xtra, 6)
-    Ydata6 = EM(Xtra, pi, mu, sigma, TransitionA)
+    Ydata6, Ydata6dev = EM(Xtra, Xdev, pi, mu, sigma, TransitionA)
 
     plt.plot(Ydata3, label = '3')
+    plt.plot(Ydata3dev, label = 'dev3')
     plt.plot(Ydata4, label = '4')
+    plt.plot(Ydata4dev, label = 'dev4')
     plt.plot(Ydata5, label = '5')
+    plt.plot(Ydata5dev, label = 'dev5')
     plt.plot(Ydata6, label = '6')
+    plt.plot(Ydata6dev, label = 'dev6')
+    plt.plot(Ydata7, label = '7')
+    plt.plot(Ydata7dev, label = 'dev7')
+    plt.plot(Ydata8, label = '8')
+    plt.plot(Ydata8dev, label = 'dev8')
     plt.legend()
     plt.show()
 
--- HW8/README.md
+++ HW8/README.md
@@ -1,1 +1,2 @@
 1.First Version, dev and func is needed.
+2. How to choose ?
